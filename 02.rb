e08529b4978e3403cbef76dc9de67c507b33ae06
Owajigbanam Ogbuluijah ✊🏾 <xigbanam@gmail.com>
2021-12-26 15:17:39 +0000

    Day 2 - Part 2

--- 02.rb
+++ 02.rb
@@ -6,15 +6,23 @@ require 'debug'
 
 filename = $PROGRAM_NAME.gsub(/\.rb$/, '')
 input = File.readlines("./#{filename}.in", chomp: true)
+            .map { |line| line.split('x').map(&:to_i) }
 
 def surface_area(l, w, h)
   2 * l * w + 2 * w * h + 2 * h * l + [l * w, w * h, h * l].min
 end
 
+def ribbon(dimensions)
+  dimensions.reduce(:*) + dimensions.sort[0, 2].sum * 2
+end
+
 def part_one(input)
-  input.map { |line| line.split('x').map(&:to_i) }
-       .map { |l, w, h| surface_area(l, w, h) }
-       .sum
+  input.map { |l, w, h| surface_area(l, w, h) }.sum
+end
+
+def part_two(input)
+  input.map { |d| ribbon(d) }.sum
 end
 
 puts part_one(input)
+puts part_two(input)
