da557b65a74603e8aa9c799873885bf0a213e7b4
Owajigbanam Ogbuluijah ✊🏾 <xigbanam@gmail.com>
2021-12-29 01:01:21 +0000

    Day 8 - Part 2

--- 08.rb
+++ 08.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-# --- Day 6: Probably a Fire Hazard ---
+# --- Day 8: Matchsticks ---
 
 require 'debug'
 
 filename = $PROGRAM_NAME.gsub(/\.rb$/, '')
 input = File.readlines("./#{filename}.in", chomp: true)
 
-puts input.map { |line| line.size - eval(line).size }.sum
+puts input.map { |line| line.inspect.size - line.size }.sum
